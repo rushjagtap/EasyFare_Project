1d0ecfc6004c6328fa545e4a42e7dedba92063ad
Antony Itteera <antonyItteera@gmail.com>
2022-03-27 12:58:58 +0100

    implemented swagger

---
+++ bus_fare/bus_email_client/src/main/java/com/tus/emailclient/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.tus.emailclient;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+	}
+}
