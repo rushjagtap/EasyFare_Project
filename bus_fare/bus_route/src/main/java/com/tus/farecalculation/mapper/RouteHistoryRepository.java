90788adecbb0514727d59fcc4188da25b2171db5
weijie <a00279875@student.ait.ie>
2022-04-04 16:19:07 +0100

    findByUserIdAndStartTime

--- bus_fare/bus_route/src/main/java/com/tus/farecalculation/mapper/RouteHistoryRepository.java
+++ bus_fare/bus_route/src/main/java/com/tus/farecalculation/mapper/RouteHistoryRepository.java
@@ -2,8 +2,17 @@ package com.tus.farecalculation.mapper;
 
 import com.tus.farecalculation.entry.BusRoute;
 import com.tus.farecalculation.entry.RouteHistory;
+import com.tus.farecalculation.entry.StopInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface RouteHistoryRepository extends JpaRepository<RouteHistory,Integer> {
+
+
+    @Query("select c from user_travel_history c where c.userId=?1 and c.tripStartTime=?2 ")
+    List<RouteHistory> findAll(Integer userId, Date  startTime);
 }
 
